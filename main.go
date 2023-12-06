5e40090259b5826305cf1cf751e70a82b0034e1f
Ricardo Viana <richard@Ricardos-MacBook-Pro.local>
2023-12-06 20:22:25 -0300

    MAIN init rotas

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 
+	"github.com/Ricardolv/mvc-api/src/controller/routes"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -11,6 +13,14 @@ func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
+	}
+
+	router := gin.Default()
+
+	routes.InitRoutes(&router.RouterGroup)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
 	}
 
 }
