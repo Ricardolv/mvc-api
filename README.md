f5132cee7f9028f8e612c287e176a48d85ac848d
Ricardo Viana <richard@Ricardos-MacBook-Pro.local>
2023-12-07 16:58:50 -0300

    MAIN finalizando atualizacao de usuario

--- README.md
+++ README.md
@@ -18,4 +18,4 @@
 - [zap logger](https://github.com/uber-go/zap)                                          <BR/>
 - [Mongo](https://pkg.go.dev/go.mongodb.org/mongo-driver@v1.13.1/mongo/options)         <BR/>
 - [Mongo drive](https://pkg.go.dev/go.mongodb.org/mongo-driver@v1.13.1/mongo/options)   <BR/>
-- [uuid](https://github.com/google/uuid)
+- [uuid](https://github.com/google/uuid)                                                <BR/>
--- src/controller/update_user.go
+++ src/controller/update_user.go
@@ -1,5 +1,57 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-func (uc *userControllerInterface) Update(c *gin.Context) {}
+	"github.com/Ricardolv/mvc-api/src/config/logger"
+	"github.com/Ricardolv/mvc-api/src/config/rest_err"
+	"github.com/Ricardolv/mvc-api/src/config/validation"
+	"github.com/Ricardolv/mvc-api/src/controller/request"
+	"github.com/Ricardolv/mvc-api/src/model"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.uber.org/zap"
+)
+
+func (uc *userControllerInterface) Update(c *gin.Context) {
+	logger.Info("Init updateUser controller",
+		zap.String("journey", "updateUser"),
+	)
+	var userRequest request.UserUpdateRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Error trying to validate user info", err,
+			zap.String("journey", "updateUser"))
+		errRest := validation.ValidateUserError(err)
+
+		c.JSON(errRest.Code, errRest)
+		return
+	}
+
+	userId := c.Param("userId")
+	if _, err := primitive.ObjectIDFromHex(userId); err != nil {
+		errRest := rest_err.NewBadRequestError("Invalid userId, must be a hex value")
+		c.JSON(errRest.Code, errRest)
+	}
+
+	domain := model.NewUserUpdateDomain(
+		userRequest.Name,
+		userRequest.Age,
+	)
+	err := uc.service.UpdateService(userId, domain)
+	if err != nil {
+		logger.Error(
+			"Error trying to call updateUser service",
+			err,
+			zap.String("journey", "updateUser"))
+		c.JSON(err.Code, err)
+		return
+	}
+
+	logger.Info(
+		"updateUser controller executed successfully",
+		zap.String("userId", userId),
+		zap.String("journey", "updateUser"))
+
+	c.Status(http.StatusOK)
+}
