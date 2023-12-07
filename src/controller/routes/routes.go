8f1ec7c1e81a0c29c7f66a62e07e1916f5c49210
Ricardo Viana <richard@Ricardos-MacBook-Pro.local>
2023-12-07 19:45:41 -0300

    MAIN MIDDLEWARE JWT

--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -2,18 +2,20 @@ package routes
 
 import (
 	"github.com/Ricardolv/mvc-api/src/controller"
+	"github.com/Ricardolv/mvc-api/src/model"
 	"github.com/gin-gonic/gin"
 )
 
-func InitRoutes(r *gin.RouterGroup,
+func InitRoutes(
+	r *gin.RouterGroup,
 	controller controller.UserControllerInterface,
 ) {
 
-	r.GET("/users/:id", controller.FindByID)
-	r.GET("/users/email/:email", controller.FindByEmail)
-	r.POST("/users", controller.Create)
-	r.PUT("/users/:id", controller.Update)
-	r.DELETE("/users/:id", controller.Delete)
+	r.GET("/users/:id", model.VerifyTokenMiddleware, controller.FindByID)
+	r.GET("/users/email/:email", model.VerifyTokenMiddleware, controller.FindByEmail)
+	r.POST("/users", model.VerifyTokenMiddleware, controller.Create)
+	r.PUT("/users/:id", model.VerifyTokenMiddleware, controller.Update)
+	r.DELETE("/users/:id", model.VerifyTokenMiddleware, controller.Delete)
 
 	r.POST("/login", controller.LoginUser)
 
